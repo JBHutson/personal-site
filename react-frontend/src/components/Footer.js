4b415837c1b65f03f3feec0e667cc9d93e9a61d0
JBHutson <hutsonjb@gmail.com>
2020-09-18 00:34:05 -0400

    added material-ui and anime dependencies

--- react-frontend/src/components/Footer.js
+++ react-frontend/src/components/Footer.js
@@ -4,6 +4,7 @@ import react from './React.png'
 import post from './Postgres.png'
 import flask from './Flask.png'
 import git from './Octocat.png'
+import mat from './Material-ui.png'
 import './Footer.css'
 
 
@@ -22,6 +23,7 @@ class Footer extends Component {
                             <Link to="/tutorial" className="link">Tutorials</Link>
                         </nav>
                         <nav className="picLinks">
+                            <a href="https://material-ui.com" title="Designed with Material-UI" className="img"><img src={mat}></img></a>
                             <a href="https://reactjs.org" title="Frontend with React" className="img"><img src={react}></img></a>
                             <a href="https://flask.palletsprojects.com/en/1.1.x/" title="Backend with Flask" className="img"><img src={flask}></img></a>
                             <a href="https://www.postgresql.org" title="Database with Postgres" className="img"><img src={post}></img></a>
